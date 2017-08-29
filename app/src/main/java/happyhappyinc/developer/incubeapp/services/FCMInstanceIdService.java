0c10e7f8969dea35c9096dd58b1ef3c0ada38629
DASAROLLOHAPPYHAPPY <ti.ven97@hotmail.com>
2017-08-29 09:25:47 -0500

    firebase

---
+++ app/src/main/java/happyhappyinc/developer/incubeapp/services/FCMInstanceIdService.java
@@ -0,0 +1,39 @@
+package happyhappyinc.developer.incubeapp.services;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+import happyhappyinc.developer.incubeapp.preferences.PreferencesManager;
+
+/**
+ * Created by DASAROLLOHAPPYHAPPY on 29/08/2017.
+ */
+
+public class FCMInstanceIdService extends FirebaseInstanceIdService {
+    public static String mRefreshedToken = "";
+
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        mRefreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d("TEST", "Refreshed token: " + mRefreshedToken);
+
+        // Llamamos el método que almacena el token
+        sendRegistrationToPreferences(mRefreshedToken);
+    }
+
+    // recibimos el token que obtiene la aplicación la primera vez que es instalada
+    public void sendRegistrationToPreferences(String token) {
+
+        // Obtenemos una instancia del preferences para almacenar el token
+        PreferencesManager mPref = new PreferencesManager(this);
+
+        // validamos de que ya tengamos un token
+        if (!token.equals("")) {
+            // guardamos el token en el perferences
+            mPref.setToken(token);
+        }
+    }
+}
---
+++ app/src/main/java/happyhappyinc/developer/incubeapp/services/FCMService.java
@@ -0,0 +1,88 @@
+package happyhappyinc.developer.incubeapp.services;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v4.content.LocalBroadcastManager;
+import android.support.v7.app.NotificationCompat;
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+import happyhappyinc.developer.incubeapp.R;
+import happyhappyinc.developer.incubeapp.activities.MainActivity;
+
+/**
+ * Created by DASAROLLOHAPPYHAPPY on 29/08/2017.
+ */
+
+public class FCMService extends FirebaseMessagingService {
+    private String[] datas;
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        // ...
+
+        // TODO(developer): Handle FCM messages here.
+        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
+        Log.d("TEST", "From: " + remoteMessage.getFrom());
+
+        // Check if message contains a data payload.
+        if (remoteMessage.getData().size() > 0) {
+            Log.d("TEST", "Message data payload: " + remoteMessage.getData());
+        }
+
+        // Check if message contains a notification payload.
+        if (remoteMessage.getNotification() != null) {
+            Log.d("TEST", "Message Notification Body: " + remoteMessage.getNotification().getBody());
+        }
+
+        // Also if you intend on generating your own notifications as a result of a received FCM
+        // message, here is where that should be initiated. See sendNotification method below.
+
+        String titulo = remoteMessage.getNotification().getTitle();
+        String texto = remoteMessage.getNotification().getBody();
+
+
+        datas = titulo.split("|");
+        showNotification(titulo, texto);
+        //mensaje();
+        //mensajeSinPedido();
+    }
+
+    private void mensajeSinPedido() {
+        /*Intent i = new Intent(ChatFragment.MENSAJE);
+        LocalBroadcastManager.getInstance(getApplicationContext()).sendBroadcast(i);*/
+    }
+
+    private void mensaje() {
+        /*Intent i = new Intent(ChatActivity.MENSAJE);
+        LocalBroadcastManager.getInstance(getApplicationContext()).sendBroadcast(i);*/
+    }
+
+    private void showNotification(String title, String text) {
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, new Intent(this, MainActivity.class), 0);
+
+        Uri alarmSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this);
+        notificationBuilder.setContentIntent(pendingIntent);
+        notificationBuilder.setContentText(text)
+                .setContentTitle(title)
+                .setSmallIcon(R.drawable.icon)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setVibrate(new long[]{0, 300, 200, 300})
+                .setSound(alarmSound)
+                .setAutoCancel(true);
+
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(0, notificationBuilder.build());
+    }
+}
